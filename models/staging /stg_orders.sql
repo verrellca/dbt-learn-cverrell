7607d2d4a7b87c8ee0247bd3f1e058f93952c33c
Charles Verrell <charles@imaginecurve.com>
2020-11-18 14:30:58 +0000

    adding ordres customers refactor

--- models/staging /stg_orders.sql
+++ models/staging /stg_orders.sql
@@ -1,7 +1,14 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+WITH orders AS (
+    
+    SELECT
 
-from raw.jaffle_shop.orders
+       id as order_id,
+       user_id as customer_id,
+       order_date,
+       status
+
+    FROM raw.jaffle_shop.orders
+)
+
+SELECT * 
+FROM orders
